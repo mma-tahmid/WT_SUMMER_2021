e56303ab403b874e9d2799035f4003eb641c566d
mma-tahmid <65032529+mma-tahmid@users.noreply.github.com>
2021-06-05 12:25:27 +0600

    Add files via upload

---
+++ task1/registration.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>registration_form</title>
+</head>
+<body>
+<form>
+
+<table>
+
+<tr>
+
+<td>
+Full Name:
+</td>
+
+<td>
+<input type="text" name="">
+
+</td>
+</tr>
+
+
+<tr>
+
+<td>
+Email:
+</td>
+
+<td>
+<input type="text" name="" >
+
+</td>
+</tr>
+
+<tr>
+
+<td>
+Password:
+</td>
+
+<td>
+<input type="password" name="" >
+
+</td>
+</tr>
+
+
+
+<tr>
+
+<td>
+Comment:
+</td>
+
+
+<td>
+<input type="textarea"  name="" >
+
+</td>
+</tr>
+
+
+<tr>
+
+<td>
+Gender:
+</td>
+
+<td>
+<input type="radio" name="Gender" > Male
+<input type="radio" name="Gender" > Female
+<input type="radio" name="Gender" > Other
+</td>
+</tr>
+
+<tr>
+
+<td>
+Please choose a hobby
+</td>
+
+<td>
+<input type="checkbox" name="Please choose a hobby" > singing 
+<input type="checkbox" name="Please choose a hobby" > Dancing
+<input type="checkbox" name="Please choose a hobby" > Reading
+</td>
+</tr>
+
+<tr>
+
+<td>
+Please choose a file
+</td>
+
+<td>
+<input type="file" name="" > 
+
+</td>
+</tr>
+
+<tr>
+<td>
+<input type="submit" value="submit">
+<input type="reset" value="Reset">
+</td>
+
+</tr>
+
+</table>
+
+</form>
+</body>
+</html>
